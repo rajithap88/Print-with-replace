ffef9e65015f14cc59e08ead3151b453d120da9c
rajithap88 <56268090+rajithap88@users.noreply.github.com>
2020-02-27 11:47:04 -0500

    Add files via upload

---
+++ Q4.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 110;
+            int k = 11;
+            //while (n != 0)
+            //{
+
+
+            for (int i = 1; n != 0; i++)
+            {
+                if ((i % 3 == 0) & (i % 5 == 0))
+                {
+                    Console.Write("US" + " ");
+                }
+                else if ((i % 5 == 0) & (i % 7 == 0))
+                {
+                    Console.Write("SF" + " ");
+                }
+                else if ((i % 3 == 0) & (i % 7 == 0))
+                {
+                    Console.Write("UF" + " ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("U" + " ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("S" + " ");
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.Write("F" + " ");
+                }
+                else
+                {
+
+                    Console.Write(i + " ");
+                }
+                n--;
+                
+                if (n % k == 0)
+                    Console.WriteLine("\n");
+
+            }
+        }
+    }
+}
